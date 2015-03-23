6966e86051b7450bd82e49da4035c750b73daac5
Richard Neumann <r.neumann@homeinfo.de>
2015-03-23 15:09:57 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     version='0.1.0-indev',
     author='Richard Neumann',
     author_email='r.neumann@homeinfo.de',
-    packages=['termgr'],
+    packages=['termgr',
+              'termgr.controllers',
+              'termgr.db',
+              'termgr.lib'],
     license=open('LICENSE.txt').read(),
     description=('Homeinfo Terminal Manager')
     )
