2581cd558cf9bce38a1bc310f11217c73b1b006a
Richard Neumann <r.neumann@homeinfo.de>
2015-04-08 17:46:31 +0200

    fix

--- termgr/controllers/mgr.py
+++ termgr/controllers/mgr.py
@@ -13,24 +13,38 @@ __all__ = []
 class TerminalManager(WsgiController):
     """Manages terminals"""
 
-    def _list_terminals(self):
-        """Lists available terminals"""
+    def _run(self):
+        """foo"""
         cid = self._query_dict.get('cid')
+        if cid is not None:
+            cid = int(cid)
+        tid = self._query_dict.get('tid')
+        if tid is not None:
+            tid = int(tid)
         cls = self._query_dict.get('cls')
+        if cls is not None:
+            cls = int(cls)
+        action = self._query_dict.get('action')
+        if action == 'list':
+            return self._list_terminals(cid, cls)
+        elif action == 'details':
+            return self._details(cid, tid)
+        else:
+            return None
+
+    def _list_terminals(self, cid, cls):
+        """Lists available terminals"""
         if cid is None:
             if cls is None:
                 terminals = Terminal.iselect(True)  # @UndefinedVariable
             else:
-                cls = int(cls)
                 terminals = Terminal.iselect(   # @UndefinedVariable
                     Terminal._cls == cls)
         else:
-            cid = int(cid)
             if cls is None:
                 terminals = Terminal.iselect(  # @UndefinedVariable
                     Terminal.customer == cid)
             else:
-                cls = int(cls)
                 terminals = Terminal.iselect(  # @UndefinedVariable
                     (Terminal.customer == cid) & (Terminal._cls == cls))
         result = termgr()
@@ -38,3 +52,16 @@ class TerminalManager(WsgiController):
             xml_data = terminal2xml(terminal)
             result.terminal.append(xml_data)
         return result
+
+    def _details(self, cid, tid):
+        """Get details of a certain terminal"""
+        if cid is None or tid is None:
+            return None  # TODO: handle error
+        else:
+            terminal = Terminal.iget(   # @UndefinedVariable
+                (Terminal.customer == cid)
+                & (Terminal.tid == tid))
+            result = termgr()
+            result.terminal_detail = terminal2xml(terminal, cid=True,
+                                                  details=None)
+            return result
