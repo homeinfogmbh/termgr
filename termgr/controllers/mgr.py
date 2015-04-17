c5f00fe2c6a88b872de5907e510d4fe1603d3731
Richard Neumann <r.neumann@homeinfo.de>
2015-04-17 10:15:12 +0200

    Outsourced details to library

--- termgr/controllers/mgr.py
+++ termgr/controllers/mgr.py
@@ -1,64 +1,18 @@
 """Controller for terminal management"""
 
-from datetime import datetime
 from ipaddress import IPv4Address, AddressValueError
-from homeinfolib.mime import mimetype
+from peewee import DoesNotExist
 from homeinfolib.wsgi import WsgiController, Error, OK
 from homeinfo.crm.address import Address
 from terminallib.db import Terminal, Class, Domain
 from terminallib.remotectrl import RemoteController
 from ..lib.db2xml import terminal2xml
 from ..lib.termgr import termgr
-from peewee import DoesNotExist
+from ..lib.details import TerminalDetails
 
 __date__ = "25.03.2015"
 __author__ = "Richard Neumann <r.neumann@homeinfo.de>"
 __all__ = ['TerminalManager']
-
-
-class TerminalDetails():
-    """Terminal details wrapper"""
-
-    @classmethod
-    def mockup(cls, screenshot_data=None):
-        """Mockup for testing"""
-        if screenshot_data is None:
-            with open('/home/rne/asdm04.jpg', 'rb') as f:
-                screenshot_data = f.read()
-        status = 'UP'
-        uptime = 10000
-        screenshot = (datetime.now(), mimetype(screenshot_data),
-                      screenshot_data)
-        touch_events = [(datetime.now(), 0, 1, 3),
-                        (datetime.now(), 123, 423, 54)]
-        return cls(status, uptime, screenshot, touch_events)
-
-    def __init__(self, status, uptime, screenshot, touch_events):
-        """Sets detail data"""
-        self._status = status
-        self._uptime = uptime
-        self._screenshot = screenshot
-        self._touch_events = touch_events
-
-    @property
-    def status(self):
-        """Returns the status"""
-        return self._status
-
-    @property
-    def uptime(self):
-        """Returns the uptime"""
-        return self._uptime
-
-    @property
-    def screenshot(self):
-        """Returns the screenshot"""
-        return self._screenshot
-
-    @property
-    def touch_events(self):
-        """Returns the touch events"""
-        return self._touch_events
 
 
 class TerminalManager(WsgiController):
