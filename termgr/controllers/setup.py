1faeafbeecd920d4e16f2d1b130623b6bd428a59
Richard Neumann <r.neumann@homeinfo.de>
2015-04-20 10:36:11 +0200

    Fixed terminal setup

--- termgr/controllers/setup.py
+++ termgr/controllers/setup.py
@@ -40,14 +40,14 @@ class SetupController(WsgiController):
                         tid = int(tid_str)
                     except:
                         return Error('Invalid terminal ID', status=400)
-                    term = Terminal.by_ids(cid, tid)
-                    if term is not None:
-                        if operator.authorize(term):
+                    terminal = Terminal.by_ids(cid, tid, deleted=False)
+                    if terminal is not None:
+                        if operator.authorize(terminal):
                             action = self.qd.get('action')
                             if action is None:
                                 return Error('No action specified', status=400)
                             else:
-                                return self._handle(term, action)
+                                return self._handle(terminal, action)
                         else:
                             return Error('Unauthorized', status=401)
                     else:
@@ -55,13 +55,13 @@ class SetupController(WsgiController):
         else:
             return Error('Invalid credentials', status=401)
 
-    def _handle(self, term, action):
+    def _handle(self, terminal, action):
         """Handles an action for a certain
         customer id, terminal id and action
         """
         status = 200
         if action == 'location':
-            location = term.address
+            location = terminal.address
             if location is not None:
                 content_type = 'text/plain'
                 charset = 'utf-8'
@@ -69,10 +69,11 @@ class SetupController(WsgiController):
             else:
                 msg = ' '.join(['No Repository configuration',
                                 'found for terminal',
-                                '.'.join([str(term.tid), str(term.cid)])])
+                                '.'.join([str(terminal.tid),
+                                          str(terminal.cid)])])
                 return InternalServerError(msg)
         elif action == 'vpn_data':
-            packager = OpenVPNPackager(term)
+            packager = OpenVPNPackager(terminal)
             try:
                 response_body = packager.get()
             except:
@@ -82,10 +83,11 @@ class SetupController(WsgiController):
                 charset = None
             else:
                 msg = ' '.join(['No OpenVPN configuration found for terminal',
-                                '.'.join([str(term.tid), str(term.cid)])])
+                                '.'.join([str(terminal.tid),
+                                          str(terminal.cid)])])
                 return InternalServerError(msg)
         elif action == 'repo_config':
-            pacman_cfg = PacmanConfig(term)
+            pacman_cfg = PacmanConfig(terminal)
             try:
                 result = pacman_cfg.get()
             except:
@@ -97,7 +99,8 @@ class SetupController(WsgiController):
             else:
                 msg = ' '.join(['No Repository configuration',
                                 'found for terminal',
-                                '.'.join([str(term.tid), str(term.cid)])])
+                                '.'.join([str(terminal.tid),
+                                          str(terminal.cid)])])
                 return InternalServerError(msg)
         else:
             msg = ''.join(['Method "', action, '" is not implemented'])
