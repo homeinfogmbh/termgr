9628d433d0914434a49289ea0c0ec37f20b9d2a7
Richard Neumann <r.neumann@homeinfo.de>
2015-04-20 10:34:32 +0200

    Added edit and delete methods

--- termgr/controllers/mgr.py
+++ termgr/controllers/mgr.py
@@ -70,6 +70,10 @@ class TerminalManager(WsgiController):
                     return self._details(cid, tid, thumbnail=thumbnail)
             elif action == 'add':
                 return self._add(cid, tid)
+            elif action == 'modify':
+                return self._modify_terminal(cid, tid)
+            elif action == 'delete':
+                return self._delete_terminal(cid, tid)
             else:
                 return Error('Invalid action', status=400)
         else:
@@ -77,65 +81,131 @@ class TerminalManager(WsgiController):
 
     def _add(self, cid, tid):
         """Adds entities"""
-        target = self.qd.get('object')
-        if target == 'terminal':
-            if cid is None:
-                return Error('No customer ID specified', status=400)
-            if tid is None:
-                return Error('No terminal ID specified', status=400)
-            location_id = self.qd.get('location')
-            if location_id is None:
-                return Error('No location specified', status=400)
-            else:
-                try:
-                    location_id = int(location_id)
-                except ValueError:
-                    Error('location must be an integer', status=400)
-            class_id = self.qd.get('class')
-            if class_id is None:
-                return Error('No class specified', status=400)
-            else:
-                try:
-                    class_id = int(class_id)
-                except ValueError:
-                    Error('class must be an integer', status=400)
-            domain_id = self.qd.get('domain_id')
-            if domain_id is None:
-                return Error('No domain specified', status=400)
-            else:
-                try:
-                    domain_id = int(domain_id)
-                except ValueError:
-                    Error('domain must be an integer', status=400)
-            ipv4addr = self.qd.get('ipv4addr')
-            if ipv4addr is not None:
-                try:
-                    ipv4addr = IPv4Address(ipv4addr)
-                except AddressValueError:
-                    return Error('Invalid IPv4 address', status=400)
-            virtual_display = self.qd.get('virtual_display')
-            display = self._add(cid, tid, location_id, class_id,
-                                domain_id, ipv4addr=ipv4addr,
-                                virtual_display=virtual_display)
-            return display
+        if cid is None:
+            return Error('No customer ID specified', status=400)
+        if tid is None:
+            return Error('No terminal ID specified', status=400)
+        location_id = self.qd.get('location')
+        if location_id is None:
+            return Error('No location specified', status=400)
         else:
-            return Error('Invalid target', status=400)
+            try:
+                location_id = int(location_id)
+            except ValueError:
+                Error('location must be an integer', status=400)
+        class_id = self.qd.get('class')
+        if class_id is None:
+            return Error('No class specified', status=400)
+        else:
+            try:
+                class_id = int(class_id)
+            except ValueError:
+                Error('class must be an integer', status=400)
+        domain_id = self.qd.get('domain_id')
+        if domain_id is None:
+            return Error('No domain specified', status=400)
+        else:
+            try:
+                domain_id = int(domain_id)
+            except ValueError:
+                Error('domain must be an integer', status=400)
+        ipv4addr = self.qd.get('ipv4addr')
+        if ipv4addr is not None:
+            try:
+                ipv4addr = IPv4Address(ipv4addr)
+            except AddressValueError:
+                return Error('Invalid IPv4 address', status=400)
+        virtual_display = self.qd.get('virtual_display')
+        return self._add(cid, tid, location_id, class_id,
+                         domain_id, ipv4addr=ipv4addr,
+                         virtual_display=virtual_display)
 
-    def _list_terminals(self, cid=None, class_id=None):
+    def _modify(self, cid, tid):
+        """Adds entities"""
+        if cid is None:
+            return Error('No customer ID specified', status=400)
+        if tid is None:
+            return Error('No terminal ID specified', status=400)
+        location_id = self.qd.get('location')
+        if location_id is None:
+            return Error('No location specified', status=400)
+        else:
+            try:
+                location_id = int(location_id)
+            except ValueError:
+                Error('location must be an integer', status=400)
+        class_id = self.qd.get('class')
+        if class_id is None:
+            return Error('No class specified', status=400)
+        else:
+            try:
+                class_id = int(class_id)
+            except ValueError:
+                Error('class must be an integer', status=400)
+        domain_id = self.qd.get('domain_id')
+        if domain_id is None:
+            return Error('No domain specified', status=400)
+        else:
+            try:
+                domain_id = int(domain_id)
+            except ValueError:
+                Error('domain must be an integer', status=400)
+        ipv4addr = self.qd.get('ipv4addr')
+        if ipv4addr is not None:
+            try:
+                ipv4addr = IPv4Address(ipv4addr)
+            except AddressValueError:
+                return Error('Invalid IPv4 address', status=400)
+        virtual_display = self.qd.get('virtual_display')
+        return self._modify_terminal(cid, tid, location_id, class_id,
+                                     domain_id,
+                                     virtual_display=virtual_display)
+
+    def _delete(self, cid, tid):
+        """Adds entities"""
+        if cid is None:
+            return Error('No customer ID specified', status=400)
+        if tid is None:
+            return Error('No terminal ID specified', status=400)
+        revoke_vpn = self.qd.get('revoke_vpn')
+        return self._delete_terminal(cid, tid, revoke_vpn)
+
+    def _list_terminals(self, cid=None, class_id=None, deleted=False):
         """Lists available terminals"""
         if cid is None:
             if class_id is None:
-                terminals = Terminal.iselect(True)  # @UndefinedVariable
+                if deleted:
+                    terminals = Terminal.iselect(True)  # @UndefinedVariable
+                else:
+                    terminals = Terminal.iselect(  # @UndefinedVariable
+                        Terminal.deleted == 0)
             else:
-                terminals = Terminal.iselect(   # @UndefinedVariable
-                    Terminal._cls == class_id)
+                if deleted:
+                    terminals = Terminal.iselect(   # @UndefinedVariable
+                        Terminal._cls == class_id)
+                else:
+                    terminals = Terminal.iselect(   # @UndefinedVariable
+                        (Terminal._cls == class_id) &
+                        (Terminal.deleted == 0))
         else:
             if class_id is None:
-                terminals = Terminal.iselect(  # @UndefinedVariable
-                    Terminal.customer == cid)
+                if deleted:
+                    terminals = Terminal.iselect(  # @UndefinedVariable
+                        Terminal.customer == cid)
+                else:
+                    terminals = Terminal.iselect(  # @UndefinedVariable
+                        (Terminal.customer == cid) &
+                        (Terminal.deleted == 0))
             else:
-                terminals = Terminal.iselect(  # @UndefinedVariable
-                    (Terminal.customer == cid) & (Terminal._cls == class_id))
+                if deleted:
+                    terminals = Terminal.iselect(  # @UndefinedVariable
+                        (Terminal.customer == cid) &
+                        (Terminal._cls == class_id))
+                else:
+                    terminals = Terminal.iselect(  # @UndefinedVariable
+                        (Terminal.customer == cid) &
+                        (Terminal._cls == class_id) &
+                        (Terminal.deleted == 0))
         result = termgr()
         for terminal in terminals:
             xml_data = terminal2xml(terminal, cid=True)
@@ -172,38 +242,93 @@ class TerminalManager(WsgiController):
     def _add_terminal(self, cid, tid, location_id, class_id, domain_id,
                       ipv4addr=None, virtual_display=None):
         """Adds a terminal with the specified configuration"""
-        term = Terminal.by_ids(cid, tid)
-        if term is None:
-            term = Terminal()
-            term.customer = cid
-            term.tid = Terminal.gen_tid(cid, desired=tid)
-            term.ipv4addr = Terminal.gen_ipv4addr(desired=ipv4addr)
+        terminal = Terminal.by_ids(cid, tid)
+        if terminal is None:
+            terminal = Terminal()
+            terminal.customer = cid
+            terminal.tid = Terminal.gen_tid(cid, desired=tid)
+            return self._set_terminal_data(terminal, location_id=location_id,
+                                           class_id=class_id,
+                                           domain_id=domain_id,
+                                           ipv4addr=ipv4addr, vpn_gen=True,
+                                           virtual_display=virtual_display)
+        else:
+            return Error('Terminal already exists', status=400)
+
+    def _modify_terminal(self, cid, tid, location_id, class_id, domain_id,
+                         virtual_display=None):
+        """Modifies a terminal given by cid and tid"""
+        if cid is None:
+            return Error('No customer ID specified', status=400)
+        elif tid is None:
+            return Error('No terminal ID specified', status=400)
+        else:
+            terminal = Terminal.by_ids(cid, tid)
+            if terminal is None:
+                return Error('No such terminal', status=400)
+            else:
+                return self._set_terminal_data(terminal,
+                                               location_id=location_id,
+                                               class_id=class_id,
+                                               domain_id=domain_id,
+                                               vpn_gen=False,
+                                               virtual_display=virtual_display)
+
+    def _set_terminal_data(self, terminal, location_id=None, class_id=None,
+                           domain_id=None, ipv4addr=None, vpn_gen=False,
+                           virtual_display=None):
+        """Sets data on a terminal instance"""
+        if ipv4addr is not None:
+            terminal.ipv4addr = Terminal.gen_ipv4addr(desired=ipv4addr)
+        if location_id is not None:
             try:
                 location = Address.iget(  # @UndefinedVariable
                     Address.id == location_id)  # @UndefinedVariable
             except DoesNotExist:
                 return Error('No such location', status=400)
-            term._location = location
+            terminal._location = location
+        if class_id is not None:
             try:
                 class_ = Class.iget(  # @UndefinedVariable
                     Class.id == class_id)  # @UndefinedVariable
             except DoesNotExist:
                 return Error('No such class', status=400)
-            term._cls = class_
+            terminal._cls = class_
+        if domain_id is not None:
             try:
                 domain = Domain.iget(  # @UndefinedVariable
                     Domain.id == domain_id)  # @UndefinedVariable
             except DoesNotExist:
-                return Error('No such class', status=400)
-            term._domain = domain
-            term.virtual_display = virtual_display
-            vpn_gen = term.gen_vpn_keys()
-            if vpn_gen:
-                term.isave()
-                xml_data = termgr()
-                xml_data.terminal = [terminal2xml(term, cid=True)]
+                return Error('No such domain', status=400)
+            terminal._domain = domain
+        if virtual_display is not None:
+            terminal.virtual_display = virtual_display
+        if vpn_gen:
+            okay = terminal.gen_vpn_keys()
+        else:
+            okay = True
+        try:
+            terminal.isave()
+        except:
+            return Error('Could not apply changes', status=500)
+        else:
+            xml_data = termgr()
+            xml_data.terminal = [terminal2xml(terminal, cid=True)]
+            if okay:
                 return OK(xml_data, content_type='application/xml')
             else:
-                Error('Could not generate OpenVPN keys', status=500)
+                return Error(xml_data, content_type='application/xml',
+                             status=500)
+
+    def _delete_terminal(self, cid, tid, revoke_vpn=True):
+        """Delete a terminal from the terminals list"""
+        terminal = Terminal.by_ids(cid, tid, deleted=True)
+        if terminal:
+            if terminal.deleted:
+                return Error('Terminal already deleted', status=200)
+            else:
+                terminal.deleted = True
+                terminal.isave()
+                return OK('Terminal deleted')
         else:
-            return Error('Terminal already exists', status=400)
+            return Error('No such terminal', status=400)
