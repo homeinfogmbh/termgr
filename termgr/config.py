6c17d3b361f7e9de8f8b4f58d07dbeee6659465d
Richard Neumann <r.neumann@homeinfo.de>
2015-06-22 13:43:32 +0200

    Fixed configuration

--- termgr/config.py
+++ termgr/config.py
@@ -8,42 +8,29 @@ __all__ = ['TermgrConfig']
 class TermgrConfig(Configuration):
     """Configuration parser enhancement"""
 
-    def __init__(self, file):
-        """Initializes the main configuration file parser"""
-        super().__init__()
-        self._file = file
-
-    def __enter__(self):
-        """Loads the configuration"""
-        self.load()
-        return self
-
-    def __exit__(self, *_):
-        """Exists the with statement"""
-        pass
-
-    def load(self):
-        """Reads the configuration file"""
-        return self.read(self._file)
-
     @property
     def monitoring(self):
+        self.load()
         return self['monitoring']
 
     @property
     def openvpn(self):
+        self.load()
         return self['openvpn']
 
     @property
     def pacman(self):
+        self.load()
         return self['pacman']
 
     @property
     def ssh(self):
+        self.load()
         return self['ssh']
 
     @property
     def screenshot(self):
+        self.load()
         return self['screenshot']
 
 termgr_config = TermgrConfig('/etc/termgr.conf')
