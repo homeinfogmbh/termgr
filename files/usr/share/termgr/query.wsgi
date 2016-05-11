0ff5dec7527cbd0f4f3d936e6ec4f659901467f6
Richard Neumann <r.neumann@homeinfo.de>
2016-05-11 17:55:17 +0200

    enhancement

---
+++ files/usr/share/termgr/query.wsgi
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+"""Terminal query interface"""
+
+from termgr.controllers.query import TerminalQuery
+
+application = TerminalQuery()
--- setup.py
+++ setup.py
@@ -4,29 +4,32 @@ from distutils.core import setup
 
 setup(
     name='termgr',
-    version='1.2.0-1',
+    version='1.3.0-1',
     author='Richard Neumann',
     author_email='r.neumann@homeinfo.de',
-    requires=['docopt',
-              'homeinfo.lib',
-              'homeinfo.crm',
-              'homeinfo.terminals',
-              'hipster'],
-    packages=['termgr',
-              'termgr.controllers',
-              'termgr.lib'],
-    data_files=[('/etc', ['files/etc/termgr.conf']),
-                ('/usr/bin', [
-                    'files/usr/bin/termgr',
-                    'files/usr/bin/termadm']),
-                ('/usr/share/termgr',
-                 ['files/usr/share/termgr/mgr.wsgi',
-                  'files/usr/share/termgr/setup.wsgi',
-                  'files/usr/share/termgr/stats.wsgi']),
-                ('/etc/uwsgi/apps-available',
-                 ['files/etc/uwsgi/apps-available/termgr.ini',
-                  'files/etc/uwsgi/apps-available/termsetup.ini',
-                  'files/etc/uwsgi/apps-available/termstats.ini'])],
-    license=open('LICENSE.txt').read(),
-    description=('Homeinfo Terminal Manager')
-    )
+    requires=[
+        'docopt',
+        'homeinfo.lib',
+        'homeinfo.crm',
+        'homeinfo.terminals',
+        'hipster'],
+    packages=[
+        'termgr',
+        'termgr.controllers',
+        'termgr.lib'],
+    data_files=[
+        ('/etc', ['files/etc/termgr.conf']),
+        ('/usr/bin', [
+            'files/usr/bin/termgr',
+            'files/usr/bin/termadm']),
+        ('/usr/share/termgr', [
+            'files/usr/share/termgr/mgr.wsgi',
+            'files/usr/share/termgr/query.wsgi',
+            'files/usr/share/termgr/setup.wsgi',
+            'files/usr/share/termgr/stats.wsgi']),
+        ('/etc/uwsgi/apps-available', [
+            'files/etc/uwsgi/apps-available/termgr.ini',
+            'files/etc/uwsgi/apps-available/termquery.ini',
+            'files/etc/uwsgi/apps-available/termsetup.ini',
+            'files/etc/uwsgi/apps-available/termstats.ini'])],
+    description=('Homeinfo Terminal Manager'))
