e4a28895ca777462ca3194015d7a8c8256e4830b
Richard Neumann <mail@richard-neumann.de>
2016-06-07 17:59:50 +0200

    Added filter.py library

---
+++ termgr/lib/filter.py
@@ -0,0 +1,25 @@
+"""Terminal filtering"""
+
+__all__ = ['deployed', 'online']
+
+
+def deployed(terminals):
+    """Filters only deployed terminals"""
+
+    NULL = timedelta(0)
+
+    for terminal in terminals:
+        if terminal.deployed is None:
+            logger.warning('Terminal {0} is not deployed'.format(terminal))
+        elif terminal.deployed - datetime.now() > NULL:
+            logger.warning('Terminal {0} is not yet deployed'.format(terminal))
+        else:
+            yield terminal
+
+
+def online(terminals):
+    """Yields terminals that are online"""
+
+    for terminal in terminals:
+        if terminal.status:
+            yield terminal
