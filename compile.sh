e393258c888330024f1e599ca63182a46c357490
Richard Neumann <r.neumann@homeinfo.de>
2016-06-10 10:57:57 +0200

    fix

--- compile.sh
+++ compile.sh
@@ -7,15 +7,8 @@ PYTHON_SCRIPT=$1
 TARGET_BIN=$2
 shift 2
 
-
 echo "Compiling: ${PYTHON_SCRIPT}"
-
-if [ "$(file -bi ${PYTHON_SCRIPT})" == "text/x-python; charset=us-ascii" ]; then
-	TMP=$(mktemp --suffix=".c")
-	cython3 -3 --embed -o ${TMP} ${PYTHON_SCRIPT}
-	gcc -Os -I ${CPYTHON} -o ${TARGET_BIN} ${TMP} ${CPYTHON_OPTS[@]}
-	rm ${TMP}
-else
-	echo "Not a python script: ${PYTHON_SCRIPT}" 1>&2
-	exit 1
-fi
+TMP=$(mktemp --suffix=".c")
+cython3 -3 --embed -o ${TMP} ${PYTHON_SCRIPT}
+gcc -Os -I ${CPYTHON} -o ${TARGET_BIN} ${TMP} ${CPYTHON_OPTS[@]}
+rm ${TMP}
