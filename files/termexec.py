554779da561efe94278611c0343f272a8f4b24d3
Richard Neumann <r.neumann@homeinfo.de>
2016-06-10 11:05:24 +0200

    fix

--- files/termexec.py
+++ files/termexec.py
@@ -17,17 +17,18 @@ from homeinfo.terminals.ctrl import RemoteController
 USER = 'termgr'
 options = docopt(__doc__)
 
-for terminal in TerminalFilter(options['<expr>']):
-    rc = RemoteController(USER, terminal)
-    result = rc.execute(options['<cmd>'])
+for expr in options['<expr>']:
+    for terminal in TerminalFilter(expr):
+        rc = RemoteController(USER, terminal)
+        result = rc.execute(options['<cmd>'])
 
-    if result:
-        with suppress(AttributeError):
-            print(result.stdout.decode())
+        if result:
+            with suppress(AttributeError):
+                print(result.stdout.decode())
 
-        exit(0)
-    else:
-        with suppress(AttributeError):
-            print(result.sterr.decode())
+            exit(0)
+        else:
+            with suppress(AttributeError):
+                print(result.sterr.decode())
 
-        exit(2)
+            exit(2)
