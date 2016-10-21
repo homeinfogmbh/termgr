ab69457ce43253215f9f0fd27d8f62fd7e492ded
Richard Neumann <r.neumann@homeinfo.de>
2016-10-21 13:24:38 +0200

    fix

--- termgr/ctrl.py
+++ termgr/ctrl.py
@@ -1,43 +1,132 @@
 """Remote terminal controller"""
 
-from homeinfo.terminals.orm import Terminal, Class
+from homeinfo.lib.log import Logger, LogLevel, TTYAnimation
+from homeinfo.lib.system import Parallelization
 from homeinfo.terminals.ctrl import RemoteController
 
 
-__all__ = ['OfflineError', 'TerminalManager']
+__all__ = ['TerminalController']
 
 
-class OfflineError(Exception):
-    """Indicates that a terminal is offline"""
+class TerminalsController():
+    """Processes several terminals in parallel"""
 
-    def __init__(self, terminal):
-        """Sets the respective terminal"""
-        self.terminal = terminal
+    def __init__(self, terminals, user=None):
+        """Sets terminals, user and logger"""
+        self.terminals = list(terminals)
+        self.user = user
+        self.logger = Logger(self.__class__.__name__, level=LogLevel.NONE)
+
+    def _reboot(self, terminal):
+        """Proxy a single terminal reboot"""
+        return TerminalController(
+            terminal, user=self.user, logger=self.logger).reboot()
+
+    def _cleanup(self, terminal):
+        """Proxy a single terminal cleanup"""
+        return TerminalController(
+            terminal, user=self.user, logger=self.logger).cleanup()
+
+    def _update(self, terminal):
+        """Proxy a single terminal upgrade"""
+        return TerminalController(
+            terminal, user=self.user, logger=self.logger).update()
+
+    def reboot(self):
+        """Reboots all terminals"""
+        with Parallelization(
+                self._reboot, self.terminals, single=True) as para:
+            with TTYAnimation(para, refresh=3):
+                para.wait()
+
+    def cleanup(self):
+        """Updates all terminals"""
+        with Parallelization(
+                self._cleanup, self.terminals, single=True) as para:
+            with TTYAnimation(para, refresh=3):
+                para.wait()
+
+    def update(self):
+        """Updates all terminals"""
+        with Parallelization(
+                self._update, self.terminals, single=True) as para:
+            with TTYAnimation(para, refresh=3):
+                para.wait()
 
 
-class TerminalManager(RemoteController):
-    """Terminal manager class"""
+class TerminalController(RemoteController):
+    """Does stuff on remote terminals"""
 
-    def __init__(self, terminal, keyfile=None, white_list=None, bl=None):
-        """Initializes the manager"""
-        super().__init__(
-            'termgr', terminal, keyfile=keyfile,
-            white_list=white_list, bl=bl)
+    def __init__(self, terminal, user=None, logger=None):
+        """Sets the respective terminal and logger"""
+        user = 'termgr' if user is None else user
+        super().__init__(user, terminal, logger=logger)
 
-    def __bool__(self):
-        """Returns the terminal's status"""
-        return self.terminal.status
+    @property
+    def resolution(self):
+        """Returns the display resolution"""
+        return self.execute('export DISPLAY=:0 \; xrandr | grep " connected"')
 
-    @classmethod
-    def add(cls, cid, class_, address=None, tid=None, domain=None):
-        """Adds a terminal with optional pre-configured address and TID"""
-        terminal = Terminal()
-        terminal.customer = cid
-        terminal.tid = Terminal.gen_tid(cid, desired=tid)
-        terminal.class_ = Class.get(Class.id == class_)
-        terminal.domain = domain if domain is not None else 1
-        terminal.ipv4addr = Terminal.gen_ipv4addr()
-        terminal.virtual_display = None
-        terminal.location = address
-        terminal.deleted = None
-        terminal.save()
+    def sudo(self, cmd, *args):
+        """Execute a command with sudo"""
+        return self.execute('/usr/bin/sudo', cmd, *args)
+
+    def pacman(self, *args):
+        """Issues a pacman command"""
+        return self.sudo('/usr/bin/pacman', '--noconfirm', *args)
+
+    def reboot(self):
+        """Reboots the terminal"""
+        return self.sudo('/usr/bin/reboot')
+
+    def cleanup(self):
+        """Cleanup local package cache"""
+        return self.pacman('-Sc')
+
+    def update(self):
+        """Update package databases"""
+        return self.pacman('-Sy')
+
+    def stage(self):
+        """Stage current packages"""
+        return self.pacman('-Suw')
+
+    def upgrade(self):
+        """Performs a system upgrade"""
+        return self.pacman('-Su')
+
+    def install(self, *pkgs, asexplicit=False):
+        """Installs software packages"""
+        options = []
+
+        if asexplicit:
+            options.append['--asexplicit']
+
+        return self.pacman(*options, '-S', *pkgs)
+
+    def remove(self, *pkgs, cascade=False, nosave=False,
+               recursive=False, unneeded=False):
+        """Removes packages"""
+        options = '-R'
+
+        if cascade:
+            options += 'c'
+
+        if nosave:
+            options += 'n'
+
+        if recursive:
+            options += 's'
+
+        if unneeded:
+            options += 'u'
+
+        return self.pacman(options, *options, *pkgs)
+
+    def unlock(self):
+        """Removes the pacman lockfile"""
+        if self.execute('/usr/bin/pidof', 'pacman'):
+            self.logger.error('Pacman is still running on {}'.format(
+                self.terminal))
+        else:
+            return self.sudo('/usr/bin/rm', '-f ', '/var/lib/pacman/db.lck')
