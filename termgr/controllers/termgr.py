198887bac6eb50ab83610307cc417a2ab7bea3d3
Richard Neumann <r.neumann@homeinfo.de>
2017-09-21 17:58:47 +0200

    Started to implement termgr API.

---
+++ termgr/controllers/termgr.py
@@ -0,0 +1,107 @@
+"""Web application for terminal management."""
+
+from json import loads
+
+from peewee import DoesNotExist
+
+from homeinfo.crm import Customer
+from homeinfo.terminals.orm import Terminal
+from peeweeplus import FieldValueError
+from wsgilib import Error, JSON, RestHandler
+
+
+class TerminalHandler(RestHandler):
+    """Handles listing and creation of terminals."""
+
+    @property
+    def text(self):
+        """Returns POST-ed text."""
+        try:
+            return self.data.decode()
+        except AttributeError:
+            raise Error('No data available.') from None
+        except ValueError:
+            raise Error('Invalid UTF-8 data.') from None
+
+    @property
+    def json(self):
+        """Returns POST-ed JSON data."""
+        try:
+            return loads(self.text)
+        except ValueError:
+            raise Error('Invalid JSON data.') from None
+
+    @property
+    def cid(self):
+        """Returns the customer ID."""
+        try:
+            return int(self.query.get('cid'))
+        except TypeError:
+            raise Error('No customer ID specified.') from None
+        except ValueError:
+            raise Error('Invalid customer ID specified.') from None
+
+    @property
+    def tid(self):
+        """Returns the terminal ID."""
+        try:
+            return int(self.resource)
+        except TypeError:
+            raise Error('No terminal ID specified.') from None
+        except ValueError:
+            raise Error('Invalid terminal ID specified.') from None
+
+    @property
+    def customer(self):
+        """Returns the respective customer."""
+        try:
+            return Customer.get(Customer.id == self.cid)
+        except DoesNotExist:
+            raise Error('No such customer.', status=404) from None
+
+    @property
+    def terminal(self):
+        """Returns a single terminal."""
+        try:
+            return Terminal.get(Terminal.id == self.tid)
+        except DoesNotExist:
+            raise Error('No such terminal.', status=404) from None
+
+    @property
+    def terminals(self):
+        """Lists available terminals."""
+        for terminal in Terminal.select(Terminal.customer == self.customer):
+            yield terminal
+
+    def get(self):
+        """Lists terminals."""
+        if self.resource is None:
+            return JSON([terminal.to_dict() for terminal in self.terminals])
+
+        return JSON(self.terminal.to_dict())
+
+    def post(self):
+        """Requests a new terminal."""
+        try:
+            terminal = Terminal.from_dict(self.json)
+        except FieldValueError as field_value_error:
+            return JSON(field_value_error.to_dict(), status=422)
+        else:
+            return JSON(terminal.to_dict())
+
+    def patch(self):
+        """Modifies terminal data."""
+        try:
+            self.terminal.patch(self.json)
+        except FieldValueError as field_value_error:
+            return JSON(field_value_error.to_dict(), status=422)
+        else:
+            return JSON(terminal.to_dict())
+
+
+class TerminalManager(RestHandler):
+    """Generates OpenVPN keys for the respective terminal."""
+
+    def get(self):
+        """Returns an OpenVPN key pair for the respective terminal."""
+        return self.generate_key()
