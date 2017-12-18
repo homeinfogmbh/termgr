2841e12dcb022520d98828d4b7a48c6db4240dcd
Richard Neumann <r.neumann@homeinfo.de>
2017-12-18 15:36:41 +0100

    Entered debug mode.

--- termgr/wsgi/__init__.py
+++ termgr/wsgi/__init__.py
@@ -7,7 +7,7 @@ from termgr.wsgi import check, query, setup
 __all__ = ['APPLICATION']
 
 
-APPLICATION = Application('termgr')
+APPLICATION = Application('termgr', debug=True)
 
 for route, method, function in check.ROUTES + query.ROUTES + setup.ROUTES:
     APPLICATION.route(route, methods=[method])(function)
