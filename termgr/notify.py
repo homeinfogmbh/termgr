35c4974944feafb9b54afc56b1721bd8a0c85233
Richard Neumann <r.neumann@homeinfo.de>
2018-04-04 10:30:13 +0200

    Updated notify.py.

--- termgr/notify.py
+++ termgr/notify.py
@@ -16,32 +16,6 @@ LOGGER = getLogger(__file__)
 MAILER = Mailer(
     CONFIG['mail']['host'], CONFIG['mail']['port'],
     CONFIG['mail']['user'], CONFIG['mail']['passwd'])
-SEP = ';'
-
-
-def terminal_fields(terminal):
-    """Yields fields of the terminal for the CSV file."""
-
-    address = None
-
-    if terminal.location:
-        address = terminal.location.address
-
-    fields = (str(terminal.tid), terminal.customer.cid)
-
-    if address is not None:
-        fields += (address.street or '', address.house_number or '',
-                   address.zip_code or '', address.city or '')
-    else:
-        fields += ('', '', '', '')
-
-    return fields
-
-
-def terminal_to_csv_record(terminal, sep=SEP):
-    """Converts the terminal into a CSV record."""
-
-    return sep.join(terminal_fields(terminal))
 
 
 def mail_terminals(user):
@@ -60,10 +34,11 @@ def mail_terminals(user):
 
         for terminal in watchlist.terminals:
             terminals.append(terminal)
-            lines.append(terminal_to_csv_record(terminal))
+            lines.append(TerminalCSVRecord.from_terminal(terminal))
 
         if lines:
-            csv_file = '\r\n'.join(lines)   # Use DOS line breaks.
+            # Use DOS line breaks for compatibility with Windows systems.
+            csv_file = '\r\n'.join(str(line) for line in lines)
             attachment = MIMEApplication(csv_file.encode(), Name=filename)
             email.attach(attachment)
             empty = False
