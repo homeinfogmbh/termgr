13b86de9cd0916fa0d496381dc219f8e30df6702
Richard Neumann <r.neumann@homeinfo.de>
2018-06-29 12:46:08 +0200

    Unified log format.

--- termgr/config.py
+++ termgr/config.py
@@ -2,6 +2,8 @@
 
 from configlib import INIParser
 
-__all__ = ['CONFIG']
+__all__ = ['CONFIG', 'LOG_FORMAT']
+
 
 CONFIG = INIParser('/etc/termgr.conf')
+LOG_FORMAT = '[%(levelname)s] %(name)s: %(message)s'
