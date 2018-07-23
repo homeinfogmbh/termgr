550ac4e2442b50912fe54367996282db521fc164
Richard Neumann <r.neumann@homeinfo.de>
2018-07-23 16:31:19 +0200

    Outsourced ordering system to own micro service.

--- termgr/wsgi/__init__.py
+++ termgr/wsgi/__init__.py
@@ -2,13 +2,11 @@
 
 from wsgilib import Application
 
-from termgr.wsgi import administer, check, order, query, setup
+from termgr.wsgi import administer, check, query, setup
 
 __all__ = ['ROUTES', 'APPLICATION']
 
 
-ROUTES = (
-    administer.ROUTES + check.ROUTES + order.ROUTES + query.ROUTES
-    + setup.ROUTES)
+ROUTES = administer.ROUTES + check.ROUTES + query.ROUTES + setup.ROUTES
 APPLICATION = Application('termgr', cors=True, debug=True)
 APPLICATION.add_routes(ROUTES)
