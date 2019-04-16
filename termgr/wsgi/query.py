ff49025cdbeaafbe4ddc3548974d15bdc5f49d1d
Richard Neumann <r.neumann@homeinfo.de>
2019-04-16 16:37:30 +0200

    Refac.

--- termgr/wsgi/query.py
+++ termgr/wsgi/query.py
@@ -3,42 +3,14 @@
 from datetime import datetime
 
 from flask import request
-from terminallib import Terminal
+from terminallib import System
 
-from wsgilib import JSON, XML, Error
+from wsgilib import JSON, Error
 
-from termgr import dom
 from termgr.auth import authorize
 from termgr.wsgi.common import authenticated
 
 __all__ = ['ROUTES']
-
-
-def terminals_to_dom(terminals):
-    """Returns terminals as XML response."""
-
-    terminals_dom = dom.terminals()
-
-    for terminal in terminals:
-        terminal_dom = dom.Terminal()
-        terminal_dom.tid = terminal.tid
-        terminal_dom.cid = terminal.customer.id
-        terminal_dom.scheduled = terminal.scheduled
-        terminal_dom.deployed = terminal.deployed
-        terminal_dom.annotation = terminal.annotation
-        address = terminal.address
-
-        if address:
-            address_dom = dom.Address()
-            address_dom.street = address.street
-            address_dom.house_number = address.house_number
-            address_dom.zip_code = address.zip_code
-            address_dom.city = address.city
-            terminal_dom.address = address_dom
-
-        terminals_dom.terminal.append(terminal_dom)
-
-    return terminals_dom
 
 
 def get_scheduled():
@@ -57,37 +29,19 @@ def get_scheduled():
     return scheduled.date()
 
 
-def get_terminals(account):
-    """List terminals of customer with CID."""
+def get_systems(account):
+    """List systems of customer with CID."""
 
-    scheduled = get_scheduled()
-    cid = request.args['cid']
-    expression = Terminal.testing == 0
-
-    if cid is not None:
-        expression &= Terminal.customer == cid
-
-    if scheduled is not None:
-        expression &= Terminal.scheduled == scheduled
-
-    if request.args.get('undeployed', False):
-        expression &= Terminal.deployed >> None
-
-    for terminal in Terminal.select().where(expression):
-        if authorize(account, terminal, read=True):
-            yield terminal
+    for system in System:
+        if authorize(account, system, read=True):
+            yield system
 
 
 @authenticated
-def query_terminals(account):
-    """Lists the respective terminals."""
+def query_systems(account):
+    """Lists the respective systems."""
 
-    if request.args.get('json'):
-        return JSON([
-            terminal.to_json(short=True) for terminal
-            in get_terminals(account)])
-
-    return XML(terminals_to_dom(get_terminals(account)))
+    return JSON([system.to_json() for system in get_systems(account)])
 
 
-ROUTES = (('POST', '/query', query_terminals, 'query_terminals'),)
+ROUTES = (('POST', '/query', query_systems),)
