a7dbe41cf19a3f4a485dc278d689e3bcecb81b35
Richard Neumann <r.neumann@homeinfo.de>
2019-04-23 14:33:11 +0200

    Added customer listing endpoint.

--- termgr/wsgi/list.py
+++ termgr/wsgi/list.py
@@ -1,17 +1,18 @@
 """List systems."""
 
 from his import ACCOUNT, authenticated
+from mdb import Customer
 from terminallib import System
 from wsgilib import JSON
 
-from termgr.orm import SystemAdministrator
+from termgr.orm import CustomerAdministrator, SystemAdministrator
 
 
 __all__ = ['ROUTES']
 
 
 def get_systems():
-    """Groups the respective terminals by customers."""
+    """Yields the allowed systems."""
 
     if ACCOUNT.root:
         yield from System
@@ -22,12 +23,35 @@ def get_systems():
         yield sysadmin.system
 
 
+def get_customers():
+    """Yields the allowed customers."""
+
+    if ACCOUNT.root:
+        yield from Customer
+        return
+
+    for customer_admin in CustomerAdministrator.select().where(
+            CustomerAdministrator.account == ACCOUNT.id):
+        yield customer_admin.customer
+
+
 @authenticated
 def list_systems():
-    """Groups the respective terminals by customers."""
+    """Lists the available systems."""
 
     return JSON([
         system.to_json(cascade=True, brief=True) for system in get_systems()])
 
 
-ROUTES = (('GET', '/list', list_systems),)
+@authenticated
+def list_customers():
+    """Groups the respective terminals by customers."""
+
+    return JSON([
+        customer.to_json(company=True) for customer in get_customers()])
+
+
+ROUTES = (
+    ('GET', '/list/systems', list_systems),
+    ('GET', '/list/customers', list_customers)
+)
