1fbaffcfcfeeb7c898da38f5afebf3d3f165f261
Richard Neumann <r.neumann@homeinfo.de>
2021-01-19 13:03:03 +0100

    Refactored to directly select FK tables.

--- termgr/wsgi/listing.py
+++ termgr/wsgi/listing.py
@@ -1,15 +1,62 @@
 """List systems."""
 
+from typing import Iterable
+
 from his import ACCOUNT, authenticated, authorized
-from hwdb import System
+from hwdb import Deployment, System
+from mdb import Address, Company, Customer
 from wsgilib import JSON
 
 from flask import Response
+from peewee import JOIN
 
-from termacls import get_administerable_deployments, get_administerable_systems
+from termacls import get_deployment_admin_condition, get_system_admin_condition
 
 
 __all__ = ['ROUTES']
+
+
+def _get_deployments() -> Iterable[Deployment]:
+    """Returns the respective deployments."""
+
+    condition = get_deployment_admin_condition(ACCOUNT)
+    lpt_address = Address.alias()
+    select = Deployment.select().join(Customer, join_type=JOIN.LEFT_OUTER)
+    select = select.join(Company, join_type=JOIN.LEFT_OUTER).join_from(
+        Deployment, Address, join_type=JOIN.LEFT_OUTER,
+        on=Deployment.address == Address.id).join_from(
+        Deployment, lpt_address, join_type=JOIN.LEFT_OUTER,
+        on=Deployment.lpt_address == lpt_address.id
+    )
+    return select.where(condition)
+
+
+def _get_systems() -> Iterable[System]:
+    """Returns the respective systems."""
+
+    condition = get_system_admin_condition(ACCOUNT)
+    lpt_address = Address.alias()
+    select = System.select().join(Deployment, join_type=JOIN.LEFT_OUTER)
+    select = select.join(Customer, join_type=JOIN.LEFT_OUTER)
+    select = select.join(Company, join_type=JOIN.LEFT_OUTER)
+    select = select.join_from(
+        Deployment, Address, join_type=JOIN.LEFT_OUTER,
+        on=Deployment.address == Address.id).join_from(
+        Deployment, lpt_address, join_type=JOIN.LEFT_OUTER,
+        on=Deployment.lpt_address == lpt_address.id
+    )
+    dataset = Deployment.alias()
+    dataset_address = Address.alias()
+    dataset_lpt_address = Address.alias()
+    select = select.join_from(
+        System, dataset, join_type=JOIN.LEFT_OUTER,
+        on=System.dataset == dataset.id).join_from(
+        dataset, dataset_address, join_type=JOIN.LEFT_OUTER,
+        on=dataset.address == dataset_address.id).join_from(
+        dataset, dataset_lpt_address, join_type=JOIN.LEFT_OUTER,
+        on=dataset.lpt_address == dataset_lpt_address.id
+    )
+    return select.where(condition)
 
 
 @authenticated
@@ -18,8 +65,8 @@ def list_deployments() -> Response:
     """Lists available deployments."""
 
     return JSON([
-        deployment.to_json(systems=True, cascade=2) for deployment in
-        get_administerable_deployments(ACCOUNT)])
+        dep.to_json(systems=True, cascade=2) for dep in _get_deployments()
+    ])
 
 
 @authenticated
@@ -27,7 +74,7 @@ def list_deployments() -> Response:
 def get_system(ident: int) -> Response:
     """Lists the available systems."""
 
-    systems = get_administerable_systems(ACCOUNT).where(System.id == ident)
+    systems = _get_systems().where(System.id == ident)
 
     try:
         system = systems.get()
@@ -42,9 +89,7 @@ def get_system(ident: int) -> Response:
 def list_systems() -> Response:
     """Lists the available systems."""
 
-    return JSON([
-        system.to_json(cascade=3, brief=True) for system in
-        get_administerable_systems(ACCOUNT)])
+    return JSON([sys.to_json(cascade=3, brief=True) for sys in _get_systems()])
 
 
 ROUTES = (
