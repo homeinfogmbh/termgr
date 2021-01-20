00d143524b1068684bc1be684c4b9b8597ee0acf
Richard Neumann <mail@richard-neumann.de>
2021-01-20 09:41:10 +0100

    Fixed joins.

--- termgr/wsgi/listing.py
+++ termgr/wsgi/listing.py
@@ -51,7 +51,6 @@ def _get_systems() -> Iterable[System]:
         System, Deployment, Address, Customer, Company, lpt_address, dataset,
         dataset_address, dataset_lpt_address
     )
-    select = System.select()
     select = select.join_from(
         System, Deployment, on=System.deployment == Deployment.id,
         join_type=JOIN.LEFT_OUTER)
