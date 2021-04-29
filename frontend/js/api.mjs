21d7015881b76ddde69753575fc5e3d2c40cfee4
Richard Neumann <mail@richard-neumann.de>
2021-04-29 14:39:53 +0200

    Update copyright year

--- frontend/js/api.mjs
+++ frontend/js/api.mjs
@@ -1,7 +1,7 @@
 /*
     api.mjs - Terminal Manager API library.
 
-    (C) 2019-2020 HOMEINFO - Digitale Informationssysteme GmbH
+    (C) 2019-2021 HOMEINFO - Digitale Informationssysteme GmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
