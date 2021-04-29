ae749ec32ecb079097473ba5755fa5c7c24915fb
Richard Neumann <mail@richard-neumann.de>
2021-04-29 14:43:10 +0200

    Update copyright year

--- frontend/js/manage.mjs
+++ frontend/js/manage.mjs
@@ -1,7 +1,7 @@
 /*
     manage.mjs - Terminal Manager systems management.
 
-    (C) 2019-2020 HOMEINFO - Digitale Informationssysteme GmbH
+    (C) 2019-2021 HOMEINFO - Digitale Informationssysteme GmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
