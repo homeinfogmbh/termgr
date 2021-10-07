3b8ed0000453d52adf6a87b18d2fd8c53f58dcc2
Richard Neumann <mail@richard-neumann.de>
2021-10-07 16:06:49 +0200

    Add system endpoint

--- termgr/wsgi/setup.py
+++ termgr/wsgi/setup.py
@@ -7,12 +7,12 @@ from os.path import basename
 from flask import Response, request
 
 from his import authenticated, authorized
-from hwdb import System, operating_system
+from hwdb import Group, System, operating_system, get_free_ipv6_address
 from wsgilib import Error, JSON, Binary
 
 from termgr.openvpn import package
 from termgr.wireguard import get_wireguard_config, reload
-from termgr.wsgi.common import admin
+from termgr.wsgi.common import admin, groupadmin
 
 
 __all__ = ['ROUTES']
@@ -89,9 +89,58 @@ def finalize(system: System) -> Response:
     return 'System finalized.'
 
 
+@authenticated
+@authorized('termgr')
+@groupadmin
+def add_system(group: Group) -> JSON:
+    """Adds a new WireGuard-only system."""
+
+    system = System(
+        group=group,
+        ipv6address=get_free_ipv6_address(),
+        pubkey=request.json['wg_pubkey'],
+        configured=datetime.now(),
+        operating_system=operating_system(request.json['os']),
+        monitor=request.json.get('monitor'),
+        serial_number=request.json.get('sn'),
+        model=request.json.get('model')
+    )
+    system.save()
+    reload()
+    return JSON(system.to_json(brief=True))
+
+
+@authenticated
+@authorized('termgr')
+@admin
+def patch_system(system: System) -> JSON:
+    """Patches the given system."""
+
+    with suppress(KeyError):
+        system.pubkey = request.json['wg_pubkey']
+
+    with suppress(KeyError):
+        system.operating_system = operating_system(request.json['os'])
+
+    with suppress(KeyError):
+        system.monitor = request.json['monitor']
+
+    with suppress(KeyError):
+        system.serial_number = request.json['sn']
+
+    with suppress(KeyError):
+        system.model = request.json['model']
+
+    system.save()
+    reload()
+    return JSON(system.to_json(brief=True))
+
+
 ROUTES = (
     ('POST', '/setup/info', get_system_info),
     ('POST', '/setup/openvpn', get_openvpn_data),
     ('POST', '/setup/wireguard', get_wireguard_data),
-    ('POST', '/setup/finalize', finalize)
+    ('POST', '/setup/finalize', finalize),
+    ('POST', '/setup/system', add_system),
+    ('PATCH', '/setup/system/<int:system>', patch_system)
 )
