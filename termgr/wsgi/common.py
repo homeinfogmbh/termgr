9ae338f02efb06fa984c91f0ac781e1a95c37d5d
Richard Neumann <mail@richard-neumann.de>
2021-10-07 15:58:09 +0200

    Add groupadmin() decorator

--- termgr/wsgi/common.py
+++ termgr/wsgi/common.py
@@ -6,13 +6,12 @@ from typing import Callable, Optional
 from flask import request
 
 from his import ACCOUNT
-from hwdb import Deployment, System
+from hwdb import Deployment, Group, System
+from termacls import can_administer_system, can_deploy, GroupAdmin
 from wsgilib import Error
 
-from termacls import can_administer_system, can_deploy
 
-
-__all__ = ['admin', 'deploy']
+__all__ = ['admin', 'deploy', 'groupadmin']
 
 
 def _get_deployment() -> Deployment:
@@ -45,6 +44,17 @@ def _get_system(system: Optional[int] = None) -> System:
         raise Error('No such system.', status=404) from None
 
 
+def _get_group(ident: int) -> Group:
+    """Returns a hardware group."""
+
+    if ACCOUNT.root:
+        return Group[ident]
+
+    return Group.select().join(GroupAdmin).where(
+        (Group.id == ident) & (GroupAdmin.account == ACCOUNT.id)
+    ).get()
+
+
 def admin(function: Callable) -> Callable:
     """Wraps the actual with admin permission checks."""
 
@@ -72,5 +82,22 @@ def deploy(function: Callable) -> Callable:
             return function(system, deployment, *args, **kwargs)
 
         raise Error('Deployment operation unauthorized.', status=403)
+
+    return wrapper
+
+
+def groupadmin(function: Callable) -> Callable:
+    """Wraps the actual group admin checks."""
+
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            group = _get_group(request.json['group'])
+        except KeyError:
+            raise Error('No group specified.') from None
+        except Group.DoesNotExist:
+            raise Error('No such group.') from None
+
+        return function(group, *args, **kwargs)
 
     return wrapper
