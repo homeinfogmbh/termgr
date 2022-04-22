31e42a7baea51badecb43bc1add0878578313173
Richard Neumann <r.neumann@homeinfo.de>
2022-04-22 13:27:53 +0200

    Simplify selects

--- termgr/wsgi/listing.py
+++ termgr/wsgi/listing.py
@@ -17,64 +17,65 @@ __all__ = ['ROUTES']
 def _get_deployments() -> ModelSelect:
     """Returns the respective deployments."""
 
-    condition = get_deployment_admin_condition(ACCOUNT)
     lpt_address = Address.alias()
-    select = Deployment.select(
-        Deployment, Customer, Company, Address, lpt_address)
-    select = select.join_from(
+    return Deployment.select(
+        Deployment, Customer, Company, Address, lpt_address
+    ).join_from(
         Deployment, Customer, on=Deployment.customer == Customer.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         Customer, Company, on=Customer.company == Company.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         Deployment, Address, on=Deployment.address == Address.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         Deployment, lpt_address,
         on=Deployment.lpt_address == lpt_address.id,
-        join_type=JOIN.LEFT_OUTER)
-    return select.where(condition)
+        join_type=JOIN.LEFT_OUTER
+    ).where(
+        get_deployment_admin_condition(ACCOUNT)
+    )
 
 
 def _get_systems() -> ModelSelect:
     """Returns the respective systems."""
 
-    condition = get_system_admin_condition(ACCOUNT)
     lpt_address = Address.alias()
     dataset = Deployment.alias()
     dataset_address = Address.alias()
     dataset_lpt_address = Address.alias()
-    select = System.select(
+    return System.select(
         System, Deployment, Address, Customer, Company, lpt_address, dataset,
         dataset_address, dataset_lpt_address
-    )
-    select = select.join_from(
+    ).join_from(
         System, Deployment, on=System.deployment == Deployment.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         Deployment, Customer, on=Deployment.customer == Customer.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         Customer, Company, on=Customer.company == Company.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         Deployment, Address, on=Deployment.address == Address.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         Deployment, lpt_address, on=Deployment.lpt_address == lpt_address.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         System, dataset, on=System.dataset == dataset.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         dataset, dataset_address, on=dataset.address == dataset_address.id,
-        join_type=JOIN.LEFT_OUTER)
-    select = select.join_from(
+        join_type=JOIN.LEFT_OUTER
+    ).join_from(
         dataset, dataset_lpt_address,
         on=dataset.lpt_address == dataset_lpt_address.id,
-        join_type=JOIN.LEFT_OUTER)
-    return select.where(condition)
+        join_type=JOIN.LEFT_OUTER
+    ).where(
+        get_system_admin_condition(ACCOUNT)
+    )
 
 
 @authenticated
