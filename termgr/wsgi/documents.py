d0bbc948a24294037c7eae871d80c09403cab8d6
Richard Neumann <r.neumann@homeinfo.de>
2022-06-02 10:42:30 +0200

    Revert to file streaming with content disposition

--- termgr/wsgi/documents.py
+++ termgr/wsgi/documents.py
@@ -1,7 +1,9 @@
 """Document download."""
 
 from pathlib import Path
-from typing import Union
+from typing import Iterator, Union
+
+from flask import Response
 
 from his import authenticated, authorized
 from wsgilib import Binary, JSONMessage
@@ -16,26 +18,40 @@ HIDSL_ARM_IMAGE = 'HIDSL-arm-raspi4-*.tar.lzop'
 MANUAL = BASEDIR / 'Installationsanleitung_DDB.pdf'
 
 
+def iter_file(path: Path, *, chunk_size: int = 4096) -> Iterator[bytes]:
+    """Yields chunks of a file."""
+
+    with path.open('rb') as file:
+        while chunk := file.read(chunk_size):
+            yield chunk
+
+
 @authenticated
 @authorized('termgr')
-def get_hidsl_iso() -> Union[Binary, JSONMessage]:
+def get_hidsl_iso() -> Union[Response, JSONMessage]:
     """Returns the latest HIDSL ISO."""
 
     for path in sorted(BASEDIR.glob(HIDSL_ISO), reverse=True):
-        with path.open('rb') as file:
-            return Binary(file.read(), filename=path.name)
+        return Response(
+            iter_file(path),
+            mimetype='application/x-iso9660-image',
+            headers={'Content-Disposition': 'filename="{path.name}"'}
+        )
 
     return JSONMessage('File not found.', status=404)
 
 
 @authenticated
 @authorized('termgr')
-def get_hidsl_arm_image() -> Union[Binary, JSONMessage]:
+def get_hidsl_arm_image() -> Union[Response, JSONMessage]:
     """Returns the latest HIDSL ARM image."""
 
     for path in sorted(BASEDIR.glob(HIDSL_ARM_IMAGE), reverse=True):
-        with path.open('rb') as file:
-            return Binary(file.read(), filename=path.name)
+        return Response(
+            iter_file(path),
+            mimetype='application/octet-stream',
+            headers={'Content-Disposition': 'filename="{path.name}"'}
+        )
 
     return JSONMessage('File not found.', status=404)
 
