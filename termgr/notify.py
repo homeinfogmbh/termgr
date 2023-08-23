cccc8bc2c0b7d089a12f518fa1b3a5b076067024
Richard Neumann <r.neumann@homeinfo.de>
2023-08-23 14:45:29 +0200

    Split email addresses by comma

--- termgr/notify.py
+++ termgr/notify.py
@@ -39,7 +39,9 @@ def get_mailer() -> Mailer:
 def get_admins() -> Iterable[str]:
     """Yields admins' emails."""
 
-    emails_ = get_config().get("notify", "admins", fallback="").split(",")
+    emails_ = map(
+        str.strip, get_config().get("notify", "admins", fallback="").split(",")
+    )
     return filter(None, map(lambda email: email.strip(), emails_))
 
 
